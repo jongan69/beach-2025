82330eaa79097d7bb71dcd4e545d33e91e9a4e7f
RaphaelXVII <120407614+RaphaelXVII@users.noreply.github.com>
2025-11-08 09:13:24 -0500

    Blend background color with ocean blue

--- src/shaders/floor/fragment.glsl
+++ src/shaders/floor/fragment.glsl
@@ -5,5 +5,6 @@ varying vec2 vUv;
 void main()
 {
     vec4 backgroundColor = texture(tBackground, vUv);
-    gl_FragColor = backgroundColor;
+    vec4 oceanBlue = vec4(0.2, 0.5, 0.9, 1.0);
+    gl_FragColor = mix(backgroundColor, oceanBlue, 0.6);
 }
